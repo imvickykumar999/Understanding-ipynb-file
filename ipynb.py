6533fae5212620a7065e78ce0a8d3959f5e580f6
Vicky Kumar <imvickykumar999@gmail.com>
2020-10-15 11:46:18 -0700

    Update ipynb.py

--- ipynb.py
+++ ipynb.py
@@ -1,27 +1,54 @@
-import ast, json, urllib.request as ur
+import ast
+import json
+import urllib.request as ur
+
 def ipynbinfo(info, file_name):
+    
     def call(file_name):
         if 'http' == file_name[0:4]:
+            
             print('\nPlease WAIT, content is loading from URL...\n')
             su = ur.urlopen(str(file_name)).read().decode('ascii')
-        elif '{' == file_name[0]: su = file_name
-        elif '\\' or '/' in file_name: su = open(file_name).read()
-        try: y = json.loads(str(su))
-        except: y = su
+            
+        elif '{' == file_name[0]:
+            su = file_name
+        elif '\\' or '/' in file_name:
+            su = open(file_name).read()
+            
+        try:
+            y = json.loads(str(su))
+        except:
+            y = su
         return ast.literal_eval(str(y))
+    
     def recdict(d):
-        try: box.append(d[info])
-        except Exception as e: e=e
+        try:
+            box.append(d[info])
+        except Exception as e:
+            pass
+        
         for i in list(d.values()):
             if type(i) == list:
+                
                 for j in i:
-                    if type(j) == dict: recdict(j)
-            if type(i) == dict: recdict(i)
+                    if type(j) == dict:
+                        recdict(j)
+                        
+            if type(i) == dict:
+                recdict(i)
+                
         return box
     return recdict(call(file_name))
-n, box, file_name = 130, [], input('Enter ipynb file Contents : ')
+
+n, box = 130, []
+file_name = input('Enter ipynb file Contents : ')
+
+if file_name == '':
+    file_name = 'https://raw.githubusercontent.com/imvickykumar999/vixtor/master/vixtor.ipynb'
+    
 extract = ['cell_type', 'execution_count', 'outputs', 'source']
-if file_name == '': file_name = 'https://raw.githubusercontent.com/imvickykumar999/vixtor/master/vixtor.ipynb'
-for i in ipynbinfo('source', file_name):
-    for j in i: print(j)
+for i in ipynbinfo(extract[3], file_name):
+    
+    for j in i:
+        print(j)
     print('='*n, end='\n\n')
